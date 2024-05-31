3c29f46d6733c7c9a771491285210362b6ee0ab2
AOR <556977aor@gmail.com>
2024-05-31 10:56:19 +0200

    cambio js

--- barf.js
+++ barf.js
@@ -100,7 +100,7 @@ $html.addEventListener('click', () => {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style.css">
-  <script defer type="module" src="main.js"></script>
+  <script defer src="main.js"></script>
   <title>Document</title>
 </head>
 <body>
